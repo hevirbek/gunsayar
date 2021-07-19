714fae5ccf5ef5460fc7c63663194c05158c1614
hevirbek <72341654+hevirbek@users.noreply.github.com>
2021-07-19 21:16:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ console.log(begin)
 today = new Date()
 console.log(today)
 diff = today - begin
-document.getElementById("h1").innerHTML = String(parseInt(diff / (24 * 60 * 60 * 1000))) + " gün oldu"
+document.getElementById("h1").innerHTML = " " + String(parseInt(diff / (24 * 60 * 60 * 1000))) + " gün oldu"
